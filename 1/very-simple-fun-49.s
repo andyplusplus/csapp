bdc9e4b72cbdb6946437f6f11bc8df413d62c0c2
Andy Wu <andyplusplus@gmail.com>
2014-12-21 18:39:53 -0500

    add assembly, preprocessed c code

---
+++ 1/very-simple-fun-49.s
@@ -0,0 +1,25 @@
+	.file	"very-simple-fun-49.c"
+	.text
+	.globl	sum
+	.type	sum, @function
+sum:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	%edi, -4(%rbp)
+	movl	%esi, -8(%rbp)
+	movl	-8(%rbp), %eax
+	movl	-4(%rbp), %edx
+	addl	%edx, %eax
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	sum, .-sum
+	.ident	"GCC: (Ubuntu 4.8.2-19ubuntu1) 4.8.2"
+	.section	.note.GNU-stack,"",@progbits
