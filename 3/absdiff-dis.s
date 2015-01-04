0deab882f4995b2dd6174aa6de4ab5e5767eb9b0
Andy Wu <andyplusplus@gmail.com>
2015-01-04 15:53:36 -0500

    absdiff

---
+++ 3/absdiff-dis.s
@@ -0,0 +1,33 @@
+
+absdiff.o:     file format elf64-x86-64
+
+
+Disassembly of section .text:
+
+0000000000000000 <absdiff_se>:
+   0:	48 39 f7             	cmp    %rsi,%rdi
+   3:	7d 0f                	jge    14 <absdiff_se+0x14>
+   5:	48 83 05 00 00 00 00 	addq   $0x1,0x0(%rip)        # d <absdiff_se+0xd>
+   c:	01 
+   d:	48 89 f0             	mov    %rsi,%rax
+  10:	48 29 f8             	sub    %rdi,%rax
+  13:	c3                   	retq   
+  14:	48 83 05 00 00 00 00 	addq   $0x1,0x0(%rip)        # 1c <absdiff_se+0x1c>
+  1b:	01 
+  1c:	48 89 f8             	mov    %rdi,%rax
+  1f:	48 29 f0             	sub    %rsi,%rax
+  22:	c3                   	retq   
+
+0000000000000023 <gotodiff_se>:
+  23:	48 39 f7             	cmp    %rsi,%rdi
+  26:	7d 0f                	jge    37 <gotodiff_se+0x14>
+  28:	48 83 05 00 00 00 00 	addq   $0x1,0x0(%rip)        # 30 <gotodiff_se+0xd>
+  2f:	01 
+  30:	48 89 f0             	mov    %rsi,%rax
+  33:	48 29 f8             	sub    %rdi,%rax
+  36:	c3                   	retq   
+  37:	48 83 05 00 00 00 00 	addq   $0x1,0x0(%rip)        # 3f <gotodiff_se+0x1c>
+  3e:	01 
+  3f:	48 89 f8             	mov    %rdi,%rax
+  42:	48 29 f0             	sub    %rsi,%rax
+  45:	c3                   	retq   
---
+++ 3/absdiff.s
@@ -0,0 +1,102 @@
+	.file	"absdiff.c"
+	.globl	lt_cnt
+	.bss
+	.align 8
+	.type	lt_cnt, @object
+	.size	lt_cnt, 8
+lt_cnt:
+	.zero	8
+	.globl	ge_cnt
+	.align 8
+	.type	ge_cnt, @object
+	.size	ge_cnt, 8
+ge_cnt:
+	.zero	8
+	.text
+	.globl	absdiff_se
+	.type	absdiff_se, @function
+absdiff_se:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movq	%rdi, -24(%rbp)
+	movq	%rsi, -32(%rbp)
+	movq	-24(%rbp), %rax
+	cmpq	-32(%rbp), %rax
+	jge	.L2
+	movq	lt_cnt(%rip), %rax
+	addq	$1, %rax
+	movq	%rax, lt_cnt(%rip)
+	movq	-24(%rbp), %rax
+	movq	-32(%rbp), %rdx
+	subq	%rax, %rdx
+	movq	%rdx, %rax
+	movq	%rax, -8(%rbp)
+	jmp	.L3
+.L2:
+	movq	ge_cnt(%rip), %rax
+	addq	$1, %rax
+	movq	%rax, ge_cnt(%rip)
+	movq	-32(%rbp), %rax
+	movq	-24(%rbp), %rdx
+	subq	%rax, %rdx
+	movq	%rdx, %rax
+	movq	%rax, -8(%rbp)
+.L3:
+	movq	-8(%rbp), %rax
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	absdiff_se, .-absdiff_se
+	.globl	gotodiff_se
+	.type	gotodiff_se, @function
+gotodiff_se:
+.LFB1:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movq	%rdi, -24(%rbp)
+	movq	%rsi, -32(%rbp)
+	movq	-24(%rbp), %rax
+	cmpq	-32(%rbp), %rax
+	jl	.L6
+	nop
+.L7:
+	movq	ge_cnt(%rip), %rax
+	addq	$1, %rax
+	movq	%rax, ge_cnt(%rip)
+	movq	-32(%rbp), %rax
+	movq	-24(%rbp), %rdx
+	subq	%rax, %rdx
+	movq	%rdx, %rax
+	movq	%rax, -8(%rbp)
+	movq	-8(%rbp), %rax
+	jmp	.L8
+.L6:
+	movq	lt_cnt(%rip), %rax
+	addq	$1, %rax
+	movq	%rax, lt_cnt(%rip)
+	movq	-24(%rbp), %rax
+	movq	-32(%rbp), %rdx
+	subq	%rax, %rdx
+	movq	%rdx, %rax
+	movq	%rax, -8(%rbp)
+	movq	-8(%rbp), %rax
+.L8:
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1:
+	.size	gotodiff_se, .-gotodiff_se
+	.ident	"GCC: (Ubuntu 4.8.2-19ubuntu1) 4.8.2"
+	.section	.note.GNU-stack,"",@progbits
