f13f8cb361d1713a11179b184d99e786fae15103
Andy Wu <andyplusplus@gmail.com>
2015-01-10 20:06:30 -0500

    add C and assembly code for analysis

---
+++ 1-2/very-simple-fun-49-dissamblers-4analysis.s
@@ -0,0 +1,19 @@
+int sum(int x, int y){
+        return x+y;
+}
+
+very-simple-fun-49.o:     file format elf64-x86-64
+
+
+Disassembly of section .text:
+
+0000000000000000 <sum>:
+   0:	55                   	push   %rbp
+   1:	48 89 e5             	mov    %rsp,%rbp
+   4:	89 7d fc             	mov    %edi,-0x4(%rbp)
+   7:	89 75 f8             	mov    %esi,-0x8(%rbp)
+   a:	8b 45 f8             	mov    -0x8(%rbp),%eax
+   d:	8b 55 fc             	mov    -0x4(%rbp),%edx
+  10:	01 d0                	add    %edx,%eax
+  12:	5d                   	pop    %rbp
+  13:	c3                   	retq   
